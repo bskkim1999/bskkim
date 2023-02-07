4765a2b02200a968307ddf857dc31156e7f7b972
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-02-07 12:45:52 +0900

    Update practice_C.c

--- practice_C.c
+++ practice_C.c
@@ -2,21 +2,11 @@
 #include <stdio.h>
 
 
+//int LED_board = 11;
 
-int LED_board = 11;
 int main(){
 
-    wiringPiSetupGpio();
-   pinMode(LED_board, OUTPUT);
-
-
-    while(1){
-
-            digitalWrite(LED_board, HIGH); 
-            delay(500);                       
-            digitalWrite(LED_board, LOW); 
-            delay(500);  
-    }
+   printf("hello~~!!");
                       
 
     return 0;
