75a1dcfe3716be8c35df0026d62584657d9f6631
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-02-07 22:59:57 +0900

    Update practice_C.c

--- practice_C.c
+++ practice_C.c
@@ -2,23 +2,20 @@
 #include <wiringPi.h>
 
 
-
-
-
+int LED_board = 21;
 int main(){
 
-   //pinMode(LED_board, OUTPUT);
+   pinMode(LED_board, OUTPUT);
 
    while(1){
 
-        /*
+        
         digitalWrite(LED_board, HIGH);
         delay(500);
         digitalWrite(LED_board, LOW);
         delay(500);
-        */
-       printf("hello");
-        delay(1000);
+        
+       
    }
                       
 
