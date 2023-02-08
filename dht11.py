16db4076a86dfb03e5bbf1070b3ccdd5af51b582
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-02-08 16:07:03 +0900

    Update dht11.py

--- dht11.py
+++ dht11.py
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import Adafruit_DHT
-
+import time
 # Sensor should be set to Adafruit_DHT.DHT11,
 # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.DHT11
@@ -42,7 +42,12 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
+
+while(1):
+    if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
+    
+    else:
     print('Failed to get reading. Try again!')
+
+    time.sleep(1)
