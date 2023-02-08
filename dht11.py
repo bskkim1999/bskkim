e0762be4f61b8c561a001edf8df0de8707cb1176
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-02-08 16:08:17 +0900

    Update dht11.py

--- dht11.py
+++ dht11.py
@@ -45,9 +45,9 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 while(1):
     if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
     
     else:
-    print('Failed to get reading. Try again!')
+        print('Failed to get reading. Try again!')
 
     time.sleep(1)
