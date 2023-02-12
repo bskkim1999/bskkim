527442fdb3a5241bb46bdc392031cb80a65854ca
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-02-12 23:03:51 +0900

    Create servotest.c

---
+++ servotest.c
@@ -0,0 +1,36 @@
+#include <Servo.h>
+#include <wiringPi.h>
+
+ 
+int servoPin = ;
+
+Servo servo; 
+
+int angle = 0; // servo position in degrees 
+
+int main(){
+
+    wiringPiSetupGpio();    //USE BCM
+    servo.attach(servoPin); 
+
+    // scan from 0 to 180 degrees
+  for(angle = 0; angle < 180; angle++) 
+  { 
+    servo.write(angle); 
+    delay(20); 
+  } 
+  // now scan back from 180 to 0 degrees
+  for(angle = 180; angle > 0; angle--) 
+  { 
+    servo.write(angle); 
+    delay(20); 
+  } 
+
+}
+
+
+
+  
+  
+
+
