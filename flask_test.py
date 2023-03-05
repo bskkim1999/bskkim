3554227219d86df8607ed97f129c786f4db1b1ae
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-05 22:36:24 +0900

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -8,13 +8,14 @@ def a() :
 
 
 @app.route('/')                     # 기본('/') 웹주소로 요청이 오면                     
-def abc():                        #hello 함수 실행
-    return 'Hello world!!!!!'
+def a():                        #hello 함수 실행
+    return 'Hello world1!!!!!'
 
 
 
 @app.route('/abcd')
-a()
+def b():                        #hello 함수 실행
+    return 'Hello world2!!!!!'
 
 if __name__ == '__main__':          # 현재 파일 실행시 개발용 웹서버 구동
     app.run(debug=True, port=99, host='172.30.1.35') 
