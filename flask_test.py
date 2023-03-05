657256d4682fab0667c98429e70dcbbe780f7d55
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-05 22:31:38 +0900

    Create flask_test.py

---
+++ flask_test.py
@@ -0,0 +1,20 @@
+from flask import Flask              # 플라스크 모듈 호출
+
+app = Flask(__name__)               # 플라스크 앱 생성        
+
+
+def a() :
+    return "kkk"
+
+
+@app.route('/')                     # 기본('/') 웹주소로 요청이 오면                     
+def abc():                        #hello 함수 실행
+    return 'Hello world!!!!!'
+
+
+
+@app.route('/abcd')
+a()
+
+if __name__ == '__main__':          # 현재 파일 실행시 개발용 웹서버 구동
+    app.run(debug=True, port=99, host='172.30.1.35') 
