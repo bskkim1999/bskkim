ab000254e8556ced7d1adcbfbfa864035f550673
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-05 23:17:15 +0900

    Update stepmotor_with_cam_and_flask.py

--- stepmotor_with_cam_and_flask.py
+++ stepmotor_with_cam_and_flask.py
@@ -74,11 +74,10 @@ def cleanup():
 
 #========================================main task======================================
 
-if __name__ == '__main__':          # 현재 파일 실행시 개발용 웹서버 구동
-    app.run(debug=True, port=83, host='172.30.1.54') 
-  
 app = Flask(__name__)               # 플라스크 앱 생성
 
+if __name__ == '__main__':          # 현재 파일 실행시 개발용 웹서버 구동
+    app.run(debug=True, port=83, host='172.30.1.54') 
 
 @app.route('/')                     # 기본('/') 웹주소로 요청이 오면                     
 def hello():                        #hello 함수 실행
