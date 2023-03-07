b4fb14a7b8b385e1cbd6e4699c742b5378dc38dd
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-07 13:44:28 +0900

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -4,13 +4,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 
-for ii in range(1,25):
+for ii in range(1,26):
     GPIO.setup(ii, GPIO.OUT)
 
 
 
 while True:
-    for i in range(1, 25):
+    for i in range(1, 26):
         GPIO.output(i, 1)
 
 
