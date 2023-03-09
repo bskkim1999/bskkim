1f924adaf4610b64c22db7f2f0bf2c5409921f20
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-09 16:59:36 +0900

    Update ultrasonic_wave_test.py

--- ultrasonic_wave_test.py
+++ ultrasonic_wave_test.py
@@ -12,13 +12,11 @@ GPIO.setup(ECHO, GPIO.IN)
 
 GPIO.output(TRIG, 0)
 print("초음파 출력 초기화")
-time.sleep(2)
+time.sleep(1)
 
 try:
     while True:
-        GPIO.output(TRIG,0)
-        time.sleep(0.5)
-        
+       
         GPIO.output(TRIG,1)
         time.sleep(0.00001)        # 10uS의 펄스 발생을 위한 딜레이
         
@@ -26,9 +24,11 @@ try:
         
         while GPIO.input(ECHO)==0:
             start = time.time()     # Echo핀 상승 시간값 저장
+            print("a")
             
         while GPIO.input(ECHO)==1:
             stop = time.time()      # Echo핀 하강 시간값 저장
+            print("b")
             
         check_time = stop - start
         distance = check_time * 34300 / 2
