1a691e116704d266b5b7142fead01b777086e96a
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-09 21:00:55 +0900

    Update ultrasonic_wave2.c

--- ultrasonic_wave2.c
+++ ultrasonic_wave2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>                              // stdio.h 파일 포함( printf() 사용하기 위해 )
 #include <wiringPi.h>                           // wiringPi.h 파일 포함
 
-int main(void )
+int main(void)
 {
    float distance, start, stop;
 
@@ -13,19 +13,19 @@ int main(void )
    pinMode(trig, OUTPUT);                          // wiringPi GPIO 0번  = Python(BCM) 17번 
    pinMode(echo, INPUT);                           // wiringPi GPIO 1번  = Python(BCM) 18번
 
-  while(1){
+  while(1) {
   
       digitalWrite(trig,0);                        // wiringPi 0번핀을 Low로 출력
       digitalWrite(trig,1);                        // wiringPi 0번핀을 High로 출력
       delayMicroseconds(10);                    // 10마이크로초 동안 멈춘다
       digitalWrite(trig,0);
     
-      while(digitalRead(echo) == 0)                // wiringPi 1번핀을 Low일 경우
-         start = micros();                      // 마이크로초 저장
-      while(digitalRead(echo) == 1)                // wiringPi 1번핀을 High일 경우
-         stop = micros();                       // 마이크로초 저장
+      while(digitalRead(echo) == 0)  {              // wiringPi 1번핀을 Low일 경우
+         start = micros();            }          // 마이크로초 저장
+      while(digitalRead(echo) == 1) {                // wiringPi 1번핀을 High일 경우
+         stop = micros();             }          // 마이크로초 저장
 
-      distance = (stop – start) / 58;           // 시간의 차이를 이용하여 거리를 도출한다
+      distance = (stop - start) / 58;           // 시간의 차이를 이용하여 거리를 도출한다
       printf("%f \n", distance);  
       delay(1000);
   }
