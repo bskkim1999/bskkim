37950c840455d50637264647d02a0b8de258912e
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-09 18:13:33 +0900

    Update ultrasonic_wave2.c

--- ultrasonic_wave2.c
+++ ultrasonic_wave2.c
@@ -1,47 +1,52 @@
-#include<stdio.h>
-#include<wiringPi.h>
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define trigPin 19	
-#define echoPin 26
 
-//old #define trigPin 21	//gpio 5
-//old #define echoPin 4	//gpio  J16-pin3 GPIO 23
+int main (void){
 
-int main(void)
-{
-	int distance=0;
-	int pulse = 0;
-	
-	long startTime;
-	long travelTime;
-	
-	if(wiringPiSetup () == -1)
-	{
-		printf("Unable GPIO Setup"); 
-		return 1;
-	}
-		
-	pinMode (trigPin, OUTPUT);
-	pinMode (echoPin, INPUT);
-	
-	for(;;)
-	{
-		digitalWrite (trigPin, LOW);
-		usleep(2);
-		digitalWrite (trigPin, HIGH);
-		usleep(20);
-		digitalWrite (trigPin, LOW);
-		
-		while(digitalRead(echoPin) == LOW);
-		
-		startTime = micros();
-		
-		while(digitalRead(echoPin) == HIGH);
-		travelTime = micros() - startTime;
-		
-		int distance = travelTime / 58;
-		
-		printf( "Distance: %dcm\n", distance);
-		delay(200);
-	}
+
+  int trig = 23 ;
+  int echo = 24 ;
+  int start_time, end_time ;
+  float distance ;
+
+
+  if (wiringPiSetup() == -1){}
+    exit(1) ;
+
+  } 
+
+  pinMode(trig, OUTPUT) ;
+  pinMode(echo , INPUT) ;
+
+
+  while(1) {
+
+    digitalWrite(trig, LOW) ;
+
+    delay(500) ;
+
+    digitalWrite(trig, HIGH) ;
+
+    delayMicroseconds(10) ;
+
+    digitalWrite(trig, LOW) ;
+
+    while (digitalRead(echo) == 0) ;
+
+    start_time = micros() ;
+
+    while (digitalRead(echo) == 1) ;
+
+    end_time = micros() ;
+
+    distance = (end_time - start_time) / 29. / 2. ;
+
+    printf("distance %.2f cm\n", distance) ;
+
+  }
+
+  return 0 ;
+
 }
