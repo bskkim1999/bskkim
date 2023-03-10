762b45d7ef0772d742023862f634e367addc3d0a
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-10 18:21:30 +0900

    Update dc_motor.py

--- dc_motor.py
+++ dc_motor.py
@@ -13,14 +13,14 @@ GPIO.setup(ln2, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 
 enA=GPIO.PWM(16, 100)
-enA.start(100)
+enA.start(70)
 
 
 while(1):
     try:
         GPIO.output(ln1, 0)
         GPIO.output(ln2, 1)
-        GPIO.PWM(16, 100)
+        GPIO.PWM(16, 70)
 
     except:
         GPIO.cleanup()
