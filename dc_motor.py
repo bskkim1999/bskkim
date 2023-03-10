594fb110ef0725a03a6586457349846de0055b8a
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-10 18:09:39 +0900

    Update dc_motor.py

--- dc_motor.py
+++ dc_motor.py
@@ -3,19 +3,26 @@ import time
 
 ln1=20
 ln2=21
-enA=16
+enA=GPIO.PWM(16, 100);
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(ln1, GPIO.OUT)
 GPIO.setup(ln2, GPIO.OUT)
-GPIO.setup(enA, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+
+enA.start(100)
+
 
 while(1):
-    GPIO.output(ln1, 1)
-    
-    GPIO.output(ln2, 0)
-    GPIO.output(enA, 1)
+    try:
+        GPIO.output(ln1, 1)
+        GPIO.output(ln2, 0)
+        GPIO.PWM(enA, 100)
+
+    except:
+        GPIO.cleanup()
+        enA.stop()    
 
     
