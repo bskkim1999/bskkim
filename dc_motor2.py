7f9b44f738c70117bb95b5967ecbef58c3a018d1
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-14 16:55:44 +0900

    Update dc_motor2.py

--- dc_motor2.py
+++ dc_motor2.py
@@ -71,8 +71,8 @@ while True:
         #GPIO.output(power_left, 1)
         GPIO.output(power_right, 1)
 
-        GPIO.output(ln1_right, 0)
-        GPIO.output(ln2_right, 1)
+        GPIO.output(ln1_right, 1)
+        GPIO.output(ln2_right, 0)
         GPIO.output(ln3_right, 0)
         GPIO.output(ln4_right, 1)
 
