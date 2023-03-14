24e4a6c28d416186c6b1397d31f3f8ddd17d1911
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-14 16:52:31 +0900

    Update dc_motor2.py

--- dc_motor2.py
+++ dc_motor2.py
@@ -63,13 +63,13 @@ enB_right_pwm.start(0)   #(dutycycle)  0 ~ 100
 
 while True:
     try:
-        enA_left_pwm.ChangeDutyCycle(100)
-        enB_left_pwm.ChangeDutyCycle(100)
+        #enA_left_pwm.ChangeDutyCycle(100)
+        #enB_left_pwm.ChangeDutyCycle(100)
         enA_right_pwm.ChangeDutyCycle(100)
         enB_right_pwm.ChangeDutyCycle(100)
         
         GPIO.output(power_left, 1)
-        GPIO.output(power_right, 1)
+        #GPIO.output(power_right, 1)
 
         #GPIO.output(ln1_right, 0)
         #GPIO.output(ln2_right, 1)
