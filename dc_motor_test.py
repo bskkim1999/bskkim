960bba8d0268067eb535b059338e33908c8f0d2b
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-14 18:35:57 +0900

    Update dc_motor_test.py

--- dc_motor_test.py
+++ dc_motor_test.py
@@ -7,19 +7,32 @@ GPIO.setwarnings(False)
 enA=16
 ln1=20
 ln2=21
-power=26
+
+enB=8
+ln3=7
+ln4=1
+
+power_right=26
 
 GPIO.setup(enA, GPIO.OUT)
 GPIO.setup(ln1, GPIO.OUT)
 GPIO.setup(ln2, GPIO.OUT)
-GPIO.setup(power, GPIO.OUT)
+GPIO.setup(ln3, GPIO.OUT)
+GPIO.setup(ln4, GPIO.OUT)
+GPIO.setup(enB, GPIO.OUT)
+GPIO.setup(power_right, GPIO.OUT)
 
 enA_pwm=GPIO.PWM(enA, 100) 
+enB_pwm=GPIO.PWM(enB, 100)
 
 enA_pwm.start(0)
+enB_pwm.start(0)
 
 while True:
-    enA_pwm.ChangeDutyCycle(100)
-    GPIO.output(power, 1)
-    GPIO.output(ln1, 0)
-    GPIO.output(ln2, 1)
+    #enA_pwm.ChangeDutyCycle(100)
+    enB_pwm.ChangeDutyCycle(100)
+    GPIO.output(power_right, 1)
+    #GPIO.output(ln1, 0)
+    #GPIO.output(ln2, 1)
+    GPIO.output(ln3, 0)
+    GPIO.output(ln4, 1)
