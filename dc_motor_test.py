fd9288d96d64734cae88a2a801fe57ece12a0b0f
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-14 17:38:25 +0900

    Create dc_motor_test.py

---
+++ dc_motor_test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+enA=16
+ln1=20
+ln2=21
+
+GPIO.setup(enA, GPIO.OUT)
+GPIO.setup(ln1, GPIO.OUT)
+GPIO.setup(ln2, GPIO.OUT)
+
+enA_pwm=GPIO.PWM(enA, 100) 
+
+enA_pwm.start(0)
+
+while True:
+    enA_pwm.ChangeDutyCycle(100)
+    GPIO.output(ln1, 0)
+    GPIO.output(ln2, 1)
