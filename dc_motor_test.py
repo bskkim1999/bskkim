18a0ea9d185e98ae5432828919b949d48d8da26d
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-14 18:50:10 +0900

    Update dc_motor_test.py

--- dc_motor_test.py
+++ dc_motor_test.py
@@ -7,47 +7,53 @@ GPIO.setwarnings(False)
 #<<핀 헤더 설정>>
 ##뒷바퀴##
 #오른쪽 뒷바퀴
-enA=16
-ln1=20
-ln2=21
+enA_back=16
+ln1_back=20
+ln2_back=21
 #왼쪽 뒷바퀴
-enB=8
-ln3=7
-ln4=1
+enB_back=8
+ln3_back=7
+ln4_back=1
 
 power_right=26
 
 ##앞바퀴##
+#오른쪽 앞바퀴
 
-GPIO.setup(enA, GPIO.OUT)
-GPIO.setup(ln1, GPIO.OUT)
-GPIO.setup(ln2, GPIO.OUT)
-GPIO.setup(ln3, GPIO.OUT)
-GPIO.setup(ln4, GPIO.OUT)
-GPIO.setup(enB, GPIO.OUT)
+
+
+
+
+
+GPIO.setup(enA_back, GPIO.OUT)
+GPIO.setup(ln1_back, GPIO.OUT)
+GPIO.setup(ln2_back, GPIO.OUT)
+GPIO.setup(ln3_back, GPIO.OUT)
+GPIO.setup(ln4_back, GPIO.OUT)
+GPIO.setup(enB_back, GPIO.OUT)
 GPIO.setup(power_right, GPIO.OUT)
 
-enA_pwm=GPIO.PWM(enA, 100) 
-enB_pwm=GPIO.PWM(enB, 100)
+enA_pwm_back=GPIO.PWM(enA_back, 100) 
+enB_pwm_back=GPIO.PWM(enB, 100)
 
-enA_pwm.start(0)
-enB_pwm.start(0)
+enA_pwm_back.start(0)
+enB_pwm_back.start(0)
 
 while True:
     try:
-        enA_pwm.ChangeDutyCycle(100)
-        enB_pwm.ChangeDutyCycle(100)
+        enA_pwm_back.ChangeDutyCycle(100)
+        enB_pwm_back.ChangeDutyCycle(100)
         GPIO.output(power_right, 1)
         
-        GPIO.output(ln1, 0)
-        GPIO.output(ln2, 1)
+        GPIO.output(ln1_back, 0)
+        GPIO.output(ln2_back, 1)
 
-        GPIO.output(ln3, 1)
-        GPIO.output(ln4, 0)
+        GPIO.output(ln3_back, 1)
+        GPIO.output(ln4_back, 0)
 
     except:
         print("interrupt!!!!!!!!!")
         GPIO.cleanup()
-        enA_pwm.stop()
-        enB_pwm.stop()
+        enA_pwm_back.stop()
+        enB_pwm_back.stop()
         exit(1)
