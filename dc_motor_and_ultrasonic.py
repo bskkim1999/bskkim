c059896b3ec6700c8120c981c98cb65a2325466d
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-21 17:06:52 +0900

    Update dc_motor_and_ultrasonic.py

--- dc_motor_and_ultrasonic.py
+++ dc_motor_and_ultrasonic.py
@@ -145,9 +145,9 @@ def distance():
     TimeElapsed = StopTime - StartTime
     # multiply with the sonic speed (34300 cm/s)
     # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
+    distance2 = (TimeElapsed * 34300) / 2
  
-    return distance
+    return distance2
 
 def find_median():
     idx=0
