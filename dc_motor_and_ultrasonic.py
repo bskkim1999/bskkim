338219e65b496b3aa06bdfa12e5da9ddc3d24435
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-21 18:24:17 +0900

    Update dc_motor_and_ultrasonic.py

--- dc_motor_and_ultrasonic.py
+++ dc_motor_and_ultrasonic.py
@@ -230,13 +230,14 @@ while True:
             time.sleep(1)
 
             while True:
+                #회전하다가 장애물과의 거리가 70cm 이상이면 다시 앞으로 간다.
                 
                 #회전한다.
                 dc_leftback_backup()
                 dc_leftfront_backup()
                 dc_rightfront()
                 dc_rightback()
-                #회전하다가 장애물과의 거리가 70cm 이상이면 다시 앞으로 간다.
+                
                 if find_median()>=70:
                     break
 
