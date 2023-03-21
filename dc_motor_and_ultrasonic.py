ff0e5d31f43a509f8945e32fde210b6ef7082396
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-21 18:59:23 +0900

    Update dc_motor_and_ultrasonic.py

--- dc_motor_and_ultrasonic.py
+++ dc_motor_and_ultrasonic.py
@@ -227,7 +227,8 @@ while True:
             dc_leftback_backup()
             dc_leftfront_backup()
             dc_rightfront_backup()
-            time.sleep(3)
+            time.sleep(1)
+
         else:
             #전진한다.
             dc_rightback()
