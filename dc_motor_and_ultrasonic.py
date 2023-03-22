26d0fe82a500a3cfe5f75421fd4ae4b4c8a73ba0
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-22 17:02:54 +0900

    Update dc_motor_and_ultrasonic.py

--- dc_motor_and_ultrasonic.py
+++ dc_motor_and_ultrasonic.py
@@ -351,7 +351,7 @@ while True:
             dc_leftfront(100)
             dc_rightfront(100)
         
-        time.sleep(0.05)
+        time.sleep(0.1)
     
     except:
         print("interrupt!!!!!!!!!")
