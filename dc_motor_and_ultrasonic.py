2af29c03281b34f75531837d14478d4a92bba812
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-24 16:45:01 +0900

    Update dc_motor_and_ultrasonic.py

--- dc_motor_and_ultrasonic.py
+++ dc_motor_and_ultrasonic.py
@@ -317,7 +317,7 @@ while True:
         print ("right = %.1f cm" % right )
         
         
-        if mid<=40.0:
+        if mid<=40.0:  #멈추는데 감속을 고려하여 40.0으로 설정!
             print("under 30cm!!")
             dc_stop()
             """
