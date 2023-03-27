00535711d27a3ba3744dbe93db92dc107c33dcb1
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-27 21:22:03 +0900

    Update monotonic_test.py

--- monotonic_test.py
+++ monotonic_test.py
@@ -1,17 +1,9 @@
 import time
 
+start_time = time.monotonic()
+
 while True:
-    start_time = time.monotonic()
-    # 1초 대기
-    while True:
-        current_time = time.monotonic()
-        elapsed_time = current_time - start_time
-        if elapsed_time >= 1:
-            break
-    
-    print("1초마다 반복합니다.")
-
-
-
-
-    
+    current_time = time.monotonic()
+    elapsed_time = current_time - start_time
+    if elapsed_time >= 1:
+        break
