cbbff19253ed88dba1df8127f23db4c089e3bb3b
bskkim1999 <124337361+bskkim1999@users.noreply.github.com>
2023-03-27 17:36:23 +0900

    Update dc_motor_and_ultrasonic_timeMonotonic.py

--- dc_motor_and_ultrasonic_timeMonotonic.py
+++ dc_motor_and_ultrasonic_timeMonotonic.py
@@ -345,7 +345,7 @@ while True:
         print ("right = %.1f cm" % right )
         
         
-        if mid<=31.0:  #멈추는데 감속을 고려하여 31.0으로 설정!
+        if mid<=25.0:  #멈추는데 감속을 고려하여 31.0으로 설정!
             print("under 25cm!!")
             
         
