ba8371210ac58099df8550f0d7ee82f59533d3cc
Kim Bo Seok <dbjewelrydb@gmail.com>
2023-06-22 18:10:33 +0900

    Update GPIO_test.py

--- GPIO_test.py
+++ GPIO_test.py
@@ -2,19 +2,20 @@ import RPi.GPIO as GPIO
 import time
 
 pin = 21
-pin2 =  3
+#pin2 =  3
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(pin, GPIO.OUT)
-GPIO.setup(pin2, GPIO.OUT)
+#GPIO.setup(pin2, GPIO.OUT)
 
 pin_pwm = GPIO.PWM(pin, 100)  #(pin, frequency)
-pin_pwm2 = GPIO.PWM(pin2, 100)  #(pin, frequency)
+#pin_pwm2 = GPIO.PWM(pin2, 100)  #(pin, frequency)
 
 pin_pwm.start(0)    #(dutycycle)
-pin_pwm2.start(0)    #(dutycycle)
+#pin_pwm2.start(0)    #(dutycycle)
+
 i=0
 i2=0
 start_time = 0
@@ -25,11 +26,30 @@ while True:
 
         #pwm
 
+        start_time = 0
+
+        # set Trigger to HIGH
+        GPIO.output(pin, 0)
+
+        GPIO.output(pin, 1)
+
+        # set Trigger after 0.01ms to LOW
+        start_time = time.monotonic()
+
+        while True:
+            current_time = time.monotonic()
+            if current_time - start_time >= 0.00001:
+                start_time = current_time
+                break
+
+        GPIO.output(pin, 0)
+
+        """
         GPIO.output(pin, 0)
         time.sleep(0.00001)
         GPIO.output(pin, 1)
         time.sleep(0.00001)
-
+        """
 
 
 
@@ -54,9 +74,9 @@ while True:
         print("finish!!")
         GPIO.cleanup()
         GPIO.output(pin, 0)
-        GPIO.output(pin2, 0)
-        pin_pwm.stop()
-        pin_pwm2.stop()
+        #GPIO.output(pin2, 0)
+        #pin_pwm.stop()
+        #pin_pwm2.stop()
         exit(1)
 
 
